f649f55cd7f99560be66a31b7c81d6dc3059f4d7
Sofia <dannavelandia6969@gmail.com>
2025-05-24 07:00:27 -0500

    lo que fue ya fue

--- src/LaberintoUniversitario.java
+++ src/LaberintoUniversitario.java
@@ -31,6 +31,10 @@ public class LaberintoUniversitario {
         3. Calcular la suma de los valores de cada fila. 🧮
         4. BONUS: Encuentra la posición del mayor número del laberinto. 👑
         """);
+        promedioImpares(laberinto);
+        sumaValoresPorFila(laberinto);
+        posicionMayorNumero(laberinto);
+        contarParesMayoresA4(laberinto);
 
         // Aquí puedes invocar los métodos para cada uno de los puntos anteriores
         // Por ejemplo:
@@ -40,6 +44,17 @@ public class LaberintoUniversitario {
 
         scanner.close();
     }
+    public static void contarParesMayoresA4(int[][] matriz) {
+    int contador = 0;
+    for (int[] fila : matriz) {
+        for (int valor : fila) {
+            if (valor % 2 == 0 && valor > 4) {
+                contador++;
+            }
+        }
+    }
+    System.out.println("Cantidad de números pares mayores a 4: " + contador);
+}
 
     public static void mostrarMatriz(int[][] matriz) {
         for (int[] fila : matriz) {
@@ -50,6 +65,47 @@ public class LaberintoUniversitario {
         }
     }
 
+    public static void promedioImpares(int[][] matriz) {
+        int suma = 0;
+        int contador = 0;
+
+        for (int[] fila : matriz) {
+            for (int valor : fila) {
+                if (valor % 2 != 0) {
+                    suma += valor;
+                    contador++;
+                }
+            }
+        }
+
+        double promedio = (contador == 0) ? 0 : (double) suma / contador;
+        System.out.println("Promedio de números impares: " + promedio);
+    }
+    public static void sumaValoresPorFila(int[][] matriz){
+    for (int i = 0; i < matriz.length; i++) {
+        int suma = 0;
+        for (int j = 0; j < matriz[i].length; j++) {
+            suma += matriz[i][j];
+        }
+        System.out.println("La suma de los valores de la fila " + (i + 1) + " es: " + suma);
+    }
+}
+    public static void posicionMayorNumero(int[][] matriz) {
+    int max = matriz[0][0];
+    int filaMax = 0;
+    int colMax = 0;
+
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j] > max) {
+                max = matriz[i][j];
+                filaMax = i;
+                colMax = j;
+            }
+        }
+    }
+    System.out.println("El mayor número es " + max + " y está en la posición: [" + filaMax + "][" + colMax + "]");
+}
     // Aquí puedes agregar los métodos necesarios para resolver el laberinto
     // Ejemplo:
     // public static void contarParesMayoresA4(int[][] matriz) { ... }
